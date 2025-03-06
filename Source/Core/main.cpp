b5dbf31a2de1f1271ac849a5b68252eea84b3dc8
Abhijithbiju2002 <abhiju7@gmail.com>
2025-03-06 12:02:49 +0530

    c2

---
+++ Source/Core/main.cpp
@@ -0,0 +1,17 @@
+// main.cpp
+#include "../../Header/Core/GameWindowManager.h"
+
+int main() {
+	// Create our window manager instance
+	Core::GameWindowManager gameWindowManager;
+
+	// Initialize the window
+	gameWindowManager.initialize();
+
+	while (gameWindowManager.isGameRunning()) {
+		gameWindowManager.render();
+	}
+	
+	return 0;
+	
+}
